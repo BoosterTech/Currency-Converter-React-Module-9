d04115380f8cadce3f66afb95830254334ab5ce1
Dariusz Podczasik <boostertech@mail.com>
2023-08-19 22:19:35 +0100

    interactions added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
+import { useState } from "react";
 import "./App.css";
 import Form from "./Form";
+import { currencies } from "./currencies";
 
 function App() {
+  const [result, setResult] = useState();
+
+  const calculateResult = (currency, amount) => {
+    const rate = currencies.find(
+      ({ abbreviation }) => abbreviation === currency
+    ).rate;
+
+    setResult({
+      sourceAmount : +amount,
+      finalAmount : amount/rate,
+      currency,
+    });
+  };
+
   return (
     <div className="App">
-      <Form />
+      <Form 
+      result={result}
+      calculateResult={calculateResult}
+      />
     </div>
   );
 }
--- src/Form/index.js
+++ src/Form/index.js
@@ -1,52 +1,72 @@
-// import { currencies } from "../currencies"
+import { useState } from "react";
+import { currencies } from "../currencies";
 import "./style.css";
+import { Result } from "../Result";
 
-const Form = () => (
-  <form className="form">
-    <h1 className="form__header">CURRENCY CONVERTER</h1>
-    <div className="container">
-      <p>
-        <label>
-          Amount:{" "}
-          <input
-            type="number"
-            min=".01"
-            name="amount"
-            className="container__amountElement"
-            step="0.01"
-            placeholder="enter amount in PLN*"
-            required
-            autofocus
-          />
-        </label>
+const Form = ({ result, calculateResult }) => {
+  const [currency, setCurrency] = useState(currencies[0].abbreviation);
+  const [amount, setAmount] = useState();
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    calculateResult(currency, amount);
+  }
+
+  return (
+    <form className="form" onSubmit={ onSubmit }>
+      <h1 className="form__header">CURRENCY CONVERTER</h1>
+      <div className="container">
+        <p>
+          <label>
+            Amount:{" "}
+            <input
+              type="number"
+              min=".01"
+              className="container__amountElement"
+              step="0.01"
+              placeholder="enter amount in PLN*"
+              value={amount}
+              onChange={({ target }) => setAmount(target.value)}
+              required
+            />
+          </label>
+        </p>
+        <p>
+          <label>
+            Currency:
+            <select
+              name="Currency"
+              className="container__currencyElement"
+              value={currency}
+              onChange={({ target }) => setCurrency(target.value)}
+            >
+              {currencies.map((currency) => (
+                <option
+                  key={currency.abbreviation}
+                  value={currency.abbreviation}
+                >
+                  {currency.name}
+                </option>
+              ))}
+            </select>
+          </label>
+        </p>
+      </div>
+
+      <Result result={result} />
+
+      <div className="calculateButton">
+        <button type="submit" className="form__submitButton">
+          CALCULATE
+        </button>
+      </div>
+
+      <p className="form__info">
+        Currency rates taken from <strong>Google Finance</strong> website on
+        19.08.2023
       </p>
-      <p>
-        <label>
-          Currency:
-          <select name="Currency" class="container__currencyElement">
-            <option value="EUR" selected>
-              EUR - European euro
-            </option>
-            <option value="USD">USD - American dollar</option>
-            <option value="GBP">GBP - British pound</option>
-          </select>
-        </label>
-      </p>
-    </div>
-
-    <div class="form__result"> </div>
-
-    <div class="calculateButtonContainer">
-      <button type="submit" class="form__submitButton">
-        CALCULATE!
-      </button>
-    </div>
-
-    <p class="form__info">
-      Currency rates taken from <strong>Google Finance</strong> website on
-      19.08.2023
-    </p>
-  </form>
-);
+    </form>
+  );
+};
 
 export default Form;
